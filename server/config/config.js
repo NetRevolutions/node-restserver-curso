bb4035c3f495253d124ee26de8e2b3f81125074c
NetRevolutions <j_rodriguez_arruela@hotmail.com>
2018-10-09 02:00:01 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'; 
 } else {
-    urlDB = "mongodb://cafe-user:Cafe2018@ds127132.mlab.com:27132/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
